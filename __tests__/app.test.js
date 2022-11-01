b449da97513b715998c8daac980ad4fbf37c6148
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-01 14:39:46 -0700

    create test for zodiac route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,13 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const { signs } = require('../lib/signs-data');
 
-describe('example test - you should probably update me', () => {
-  it('home route should return hello world', async () => {
-    const resp = await request(app).get('/');
-    expect(resp.text).toEqual('hello world!');
+describe('zodiac route', () => {
+  it('/zodiac should return a list of zodiac signs', async () => {
+    const resp = await request(app).get('/zodiac');
+    const expected = signs.map((sign) => {
+      return { id: sign.id, name: sign.name };
+    });
+    expect(resp.body).toEqual(expected);
   });
 });
